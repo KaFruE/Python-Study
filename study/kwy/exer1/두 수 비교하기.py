f431ed10c9985f424713e705dd010cd8c785f9a4
wykim111 <40811068+wykim111@users.noreply.github.com>
2019-11-30 14:47:21 +0900

    Create 두 수 비교하기.py

---
+++ study/kwy/exer1/두 수 비교하기.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 14:07:02 2019
+
+@author: Owner
+"""
+
+a=1
+b=2
+
+print("#TC 1")
+if a >b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("==")
+    
+
+a=10
+b=2
+print("#TC 2")
+if a >b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("==")
+    
+    
+
+a=5
+b=5
+print("#TC 3")
+if a >b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("==")
