e4eec809e32ce3966be0443b94d865a3a137d6ca
wykim111 <40811068+wykim111@users.noreply.github.com>
2019-12-01 12:51:30 +0900

    Create 성적.py

---
+++ study/kwy/exer1/성적.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 12:45:58 2019
+
+@author: wykim
+"""
+
+point = 79
+
+if point >100 or point < 0:
+    print("잘못 입력하셨습니다")
+elif point >= 90:
+    print("수")
+elif point >= 80:
+    print("우")
+elif point >=70:
+    print("미")
+elif point >= 60:
+    print("양")
+else:
+    print("가")
