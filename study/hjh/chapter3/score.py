71986eecca7bc19cac4c6233768b697e5390d287
enterblack <41631935+enterblack@users.noreply.github.com>
2019-12-06 10:23:22 +0900

    Create score.py

---
+++ study/hjh/chapter3/score.py
@@ -0,0 +1,13 @@
+score = 1
+if(score < 0 or score > 100):
+	print("예외 처리")
+elif(score >= 90):
+	print("수")
+elif(score >= 80):
+	print("우")
+elif(score >= 70):
+	print("미")
+elif(score >= 60):
+	print("양")
+else:
+	print("가")
